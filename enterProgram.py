83be79d8caa479eceb1c787ef0e85e5e97574573
Jiachen Xu <jiachen2@ualberta.ca>
2020-11-21 13:15:05 -0700

    post action

--- enterProgram.py
+++ enterProgram.py
@@ -8,25 +8,30 @@
 import random
 
 def init_function(db):
-    collection = db["users"]
+    user_collection = db["users"]
+    running = True
+    while running:
+        user_id_choice = input("Would you like to select an ID?(y/n): ")
+        if(user_id_choice == "y" or user_id_choice=="Y"):
+            user_id = input("Please enter your ID number: ")
+            results = user_collection.find({"_id":user_id})
 
-    user_id_choice = input("Would you like to select an ID?(y/n): ")
-    if(user_id_choice == "y" or user_id_choice=="Y"):
-        user_id = input("Please enter your ID number: ")
-        results = collection.find({"_id":user_id})
-
-        if (not results):
+            if (not results):
+                user = {"_id":user_id}
+                user_collection.insert_one(user)
+            else:
+                for result in results:
+                    print(result)
+            break
+        
+        
+        elif(user_id_choice == "n" or user_id_choice=="N"):
+            user_id = random.randint(1,9999)
             user = {"_id":user_id}
-            collection.insert_one(user)
-        else:
-            for result in results:
-                print(result)
-        
-        
-    elif(user_id_choice == "n" or user_id_choice=="N"):
-        user = ""
-        collection.insert_one(user).insert_id        
+            user_collection.insert_one(user)    
+            break
 
-    else:
-        print("Invalid input, please try again: ")
+        else:
+            print("Invalid input, please try again: ")
+    return user_id
         
