e3b9d83db23b770efc313b70c1ebf76bdb46a0fd
Jiachen Xu <jiachen2@ualberta.ca>
2020-11-25 11:26:33 -0700

    bug fixed
    1.user with id can now only vote once to the same post
    2.user without id can vote many times as they want

--- action_vote.py
+++ action_vote.py
@@ -4,35 +4,35 @@ import random
 def action_vote(db, current_user, pid):
     votesCollection = db['Votes']
 
-    votes_collect = votesCollection.find({"$and":[{"PostId":pid},{"UserId":current_user}]})
-    votes = []
-    for i in votes_collect:
-        votes.append(i)
-
-    if len(votes) > 0:
-        print("You have already voted...")
-        return
-
-    vote_id = str(random.randint(100000000,99999999999))
+    vote_id = str(random.randint(9999999,99999999999))
     while votesCollection.find({"Id":vote_id}) == True:
-        vote_id = str(random.randint(100000000,99999999999))
+        vote_id = str(random.randint(9999999,99999999999))
 
 
     current_time = datetime.datetime.utcnow()
     
     # if user has an id
-    if current_user:
-        answer = {
+    if current_user != None:
+        votes_collect = votesCollection.find({"$and":[{"PostId":pid},{"UserId":current_user}]})
+        votes = []
+        for i in votes_collect:
+            votes.append(i)
+
+        if len(votes) > 0:
+            print("You have already voted...")
+            return
+        else:
+            answer = {
             "Id": vote_id,
             "PostId": pid,
             "VoteTypeId": "2",
             "UserId": current_user,
             "CreationDate": current_time
-        }
+            }
 
-        print("Inserting...")
-        votesCollection.insert_one(answer)
-        print("Voted successful")
+            print("Inserting...")
+            votesCollection.insert_one(answer)
+            print("Voted successful")
     # if user has no uid
     else:
         answer = {
